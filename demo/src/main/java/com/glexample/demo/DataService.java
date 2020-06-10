b2f272e1b240a67dbe56451a009f5f97cf850aee
david komolik <david.komolik@gmail.com>
2020-06-10 15:36:12 +0200

    added post mapping

--- demo/src/main/java/com/glexample/demo/DataService.java
+++ demo/src/main/java/com/glexample/demo/DataService.java
@@ -2,9 +2,33 @@ package com.glexample.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Service
 public class DataService {
-    public Book getBook() {
+    private ConcurrentHashMap<String,Book> books;
+
+    public DataService() {
+        this.books = new ConcurrentHashMap<>();
+    }
+
+    public ConcurrentHashMap<String, Book> getBooks() {
+        return books;
+    }
+
+    public Book getBook(String name) {
+        if(books.containsKey(name))
+        {
+            return books.get(name);
+        }
+        //todo error 404
         return new Book("Harry Potter", 250);
+    }
+
+    public void setBook(Book book)
+    {
+        books.put(book.getName(),book);
+        // databaza insert
+        //LOG.info("addBook: {}", book.getName());
     }
 }
--- demo/src/main/java/com/glexample/demo/DemoController.java
+++ demo/src/main/java/com/glexample/demo/DemoController.java
@@ -1,9 +1,11 @@
 package com.glexample.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
 @RequestMapping(path="/services")
@@ -16,13 +18,19 @@ public class DemoController {
         return "Hello World!";
     }
 
-    @GetMapping(path="/data1")
-    public String getData1() {
-        return "Hi Zuzanka!";
+    @GetMapping(path="/book")
+    public Book getBook(@RequestParam String name) {
+        return dataService.getBook(name);
     }
 
-    @GetMapping(path="/book")
-    public Book getBook() {
-        return dataService.getBook();
+    @GetMapping(path="/allBooks")
+    public ConcurrentHashMap getBook() {
+        return dataService.getBooks();
+    }
+
+    @PostMapping(path="/postBook")
+    public void addBook(@RequestBody Book book) {
+        //LOG.info("addBook: {}", book.getName());
+        dataService.setBook(book);
     }
 }
